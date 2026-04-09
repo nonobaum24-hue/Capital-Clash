b11918ecd9bdaf43b01138bcb349dcda9b833d9b
Noah Kiefer <nonobaum24@gmail.com>
2026-04-09 20:43:35 +0200

    nono

--- game_classes.py
+++ game_classes.py
@@ -363,7 +363,7 @@ class super_opp(opponent):
 		self.x = x
 		self.y = y
 		self.speed = 2 + uniform(-0.5, 0.5)
-		self.damage = 34
+		self.damage = 30
 
 		# Bilder laden
 		script_dir = os.path.dirname(os.path.abspath(__file__))
