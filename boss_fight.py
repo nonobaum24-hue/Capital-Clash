960f435ccf48c9c5c84f6ee8c3549f44a8c17e14
Noah Kiefer <nonobaum24@gmail.com>
2026-04-14 22:45:37 +0200

    feat: implement boss fight function and create game entry point

--- boss_fight.py
+++ boss_fight.py
@@ -1,4 +1,6 @@
-import os
-import pygame
-from game_classes import marx, health_bar, damage_area, damage_screen, SpawnManager, collectible_manager, normal_opp, super_opp, mini_opp, boss_opp, punch_area
+def boss_fight():
+    import pygame
+    import os
+    from game_classes import marx, health_bar, damage_area, damage_screen, SpawnManager, collectible_manager, normal_opp, super_opp, mini_opp, boss_opp, punch_area
 
+    
---
+++ game.py
@@ -0,0 +1,6 @@
+import os
+import pygame
+from game_classes import marx, health_bar, damage_area, damage_screen, SpawnManager, collectible_manager, normal_opp, super_opp, mini_opp, boss_opp, punch_area
+from main_game_loop import mainloop
+
+mainloop()
