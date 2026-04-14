a7e688fe2e6e26bcdd2673c7077d70a591436470
Noah Kiefer <nonobaum24@gmail.com>
2026-04-14 09:25:46 +0200

    nono

--- main_game_loop.py
+++ main_game_loop.py
@@ -169,6 +169,9 @@ while running:
 	pygame.display.flip()   # fertigen Frame auf den Bildschirm bringen
 	clock.tick(60)          # maximal 60 FPS (hält Spielgeschwindigkeit konstant)
 
+	if roundtick >= 0 and not opponents:
+		running = False
+
 # ─────────────────────────────────────────────────────────────────────────────
 # Aufräumen
 # ─────────────────────────────────────────────────────────────────────────────
