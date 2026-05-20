fe34404052db79634d0a9ed0ebdeaeb80cf47bd4
nonobaum24-hue <nonobaum24@gmail.com>
2026-05-20 21:29:52 +0200

    nono

--- settings.py
+++ settings.py
@@ -33,6 +33,29 @@ class settings:
         self.settings_file = os.path.join(os.path.dirname(__file__), "user_settings.py")
         self.load_settings()
     
+    def get_settings(self):
+        return {
+            "width": self.width,
+            "height": self.height,
+            "title": self.__title,
+            "fps": self.__fps,
+            "round_duration": self.__round_duration,
+            "round_ticks": self.__round_ticks,
+            "end_tick_buffer": self.__end_tick_buffer,
+            "player_health": self.__player_health,
+            "player_scale": self.__player_scale,
+            "player_speed": self.__player_speed,
+            "player_attack_damage": self.__player_attack_damage,
+            "player_attack_cooldown": self.__player_attack_cooldown,
+            "player_exception_radius": self.__player_exception_radius,
+            "master_volume": self.master_volume,
+            "music_volume": self.music_volume,
+            "sfx_volume": self.sfx_volume,
+            "skin": self.__skin,
+            "background_color": self.__background_color,
+            "background_opacity": self.__background_opacity
+        }
+    
     def save_settings(self):
         content = f"""# Auto-generated user settings
 width = {self.width}
@@ -43,29 +66,6 @@ sfx_volume = {self.sfx_volume}
 """
         with open(self.settings_file, 'w') as f:
             f.write(content)
-    
-    def load_settings(self):
-        if os.path.exists(self.settings_file):
-            try:
-                namespace = {}
-                with open(self.settings_file, 'r') as f:
-                    exec(f.read(), namespace)
-                self.width = namespace.get("width", 1250)
-                self.height = namespace.get("height", 720)
-                self.master_volume = namespace.get("master_volume", 50)
-                self.music_volume = namespace.get("music_volume", 50)
-                self.sfx_volume = namespace.get("sfx_volume", 50)
-            except:
-                pass
-
-    def window_settings(self, width, height):
-        self.height = height
-        self.width = width
-
-    def volume_settings(self, master_volume, music_volume, sfx_volume):
-        self.master_volume = master_volume
-        self.music_volume = music_volume
-        self.sfx_volume = sfx_volume
     
     def load_settings(self):
         if os.path.exists(self.settings_file):
