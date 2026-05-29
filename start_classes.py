8a08dee5861ae2399eaf07654450ef32e356e91a
Noah Kiefer <nonobaum24@gmail.com>
2026-05-29 15:24:57 +0200

    nono

--- start_classes.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import os
-
-# Deprecated: Nutze stattdessen die Klassen aus ui_components.py
-from ui_components import StartButton as start_button
-from ui_components import SettingsButton as settings_button
-
-__all__ = ['start_button', 'settings_button']
